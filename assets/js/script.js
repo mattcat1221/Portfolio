8cdab9937288745ff53eda9d93e43e860909968a
mattcat1221 <158774626+mattcat1221@users.noreply.github.com>
2024-09-10 18:03:50 -0400

    working on clickable links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
+document.querySelector('.h0').classList.toggle('h200');
+document.querySelector('.h200').classList.toggle('h0');
 
-document.querySelector('.details').children[1].style.height = '200px';
+let ids = ['objective','skills','education']
+
+window.onclick = ({target}) => {
+
+    if(ids.includes(target.id)) {
+        document.querySelectorAll('h2').forEach(el => {
+            el.nextElementSibling.classList.toggle('h200', false)
+            el.nextElementSibling.classList.toggle('h0', true)
+        })
+
+        target.nextElementSibling.classList.toggle('h0', false)
+        target.nextElementSibling.classList.toggle('h200', true)
+
+        console.log(target);
+        
+    }
+};
