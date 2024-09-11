51037cc0d3e215fa7d6a8d3684a7b826197a3739
mattcat1221 <158774626+mattcat1221@users.noreply.github.com>
2024-09-11 17:37:39 -0400

    working on skill bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,14 @@ window.onclick = ({target}) => {
         
     }
 };
+
+let icons = ['aws.png','api.png','beautifulSoup.png','bootstrap.png','colab.png','css2.png','d3.png','dashboaring.png','dataStructure.png','vba.png','etl.png','flask.png','geomapping.png','git.png','github.png','googleMaps.png','hadoop.png','html.png','heroku.png','javaScript.png','machineLearning.png','matplotlib.png','mysql.png','numpy.png','pandas.png','pyspark.png','splinter.png','tableau.png'];
+
+icons.forEach(icon => {
+    skillIcon.innerHTML += `
+    <figure>
+        <img src="assets/images/skills/${icon}" alt="${icon}" >
+        <figcaption>${icon.slice(0,-4)} </figcaption>
+    <figure>
+    `;
+});
